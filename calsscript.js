7d46589926060c2e0b67c3628cb0c9325b246619
Katushann <Qk58@yandex.ru>
2023-11-24 13:37:12 +0300

    first commit

---
+++ calsscript.js
@@ -0,0 +1,147 @@
+const result = document.querySelector('#result'),
+      expression = document.querySelector('#expression'),
+      num = document.querySelectorAll('.number:not(.equals)'),
+      operation = document.querySelectorAll('.operation'),
+      equals = document.querySelector('.equals'),
+      clear = document.querySelector('#clear'),
+      bs = document.querySelector('#delete1smb');
+      ms = document.querySelector('#savenumber');
+      mr = document.querySelector('#shownumber');
+        mc = document.querySelector('#clearmemory');
+        ac = document.querySelector('#all-clear');
+let ex = ''; // the expression string to be eval'd
+let memory = '';
+result.innerHTML = '0';
+
+
+
+function clickN() { // when we click on a number
+  if(!ex || typeof(ex) === 'number' || ex === '0') {
+    expression.innerHTML = this.id;
+    ex = this.id;
+  } else {
+    expression.innerHTML += this.id;
+    ex += this.id;
+  }
+  result.innerHTML = ex.split(/\/|\*|\+|-|=/).pop();
+  checkLength(result.innerHTML);
+};
+
+function clickO() { // when we click on an operation
+  if(!ex && this.id.match(/\/|\*|\+|=/)) {
+    return;
+  }
+  ex = ex.toString().replace(/=/, '');
+  if (ex.match(/\/|\*|\+|-|=/)) {
+    ex = eval(ex).toString();
+  }
+  expression.innerHTML = expression.innerHTML.replace(/=/, '') + this.id;
+  ex += this.id;
+  result.innerHTML = this.id;
+};
+
+
+
+Array.from(num).forEach(function(element) { // assign appropriate function to all numbers and operations
+      element.addEventListener('click', clickN);
+    });
+
+Array.from(operation).forEach(function(element) {
+      element.addEventListener('click', clickO);
+    });
+
+// clear all on click
+clear.addEventListener('click', () => {
+  result.innerHTML = '';
+  expression.innerHTML = '';
+  ex = '';
+})
+
+// clear last entry on click
+bs.addEventListener('click', () => {
+  if (!expression.innerHTML.match(/=$/)) {
+    
+    expression.innerHTML = doCE(expression.innerHTML);
+    ex = doCE(ex); 
+    
+    
+      function doCE(arg) {
+          result.innerHTML = arg.slice(0, arg.length - 1);
+        return arg.slice(0, arg.length - 1);
+    }
+  }
+})
+// save the last number on click
+ms.addEventListener('click', () => {
+    
+  if(!ex) {
+    return;
+  }
+  let ar = ex.split(/\/|\*|\+|-|=/);
+    if (ar.length <= 1) {
+        memory = ar[0];
+    }
+    else
+    {
+        memory = ar[ar.length - 1]
+    }
+   })
+   // show the number saved
+mr.addEventListener('click', () => {
+    
+  if (!memory) {
+    return;
+  }
+    ex += memory;
+    result.innerHTML = ex;
+    expression.innerHTML = ex;
+    checkLength(result.innerHTML);
+})
+
+// show the number saved
+mc.addEventListener('click', () => {
+
+    if (!memory) {
+        return;
+    }
+    memory = '';
+})
+ac.addEventListener('click', () => {
+
+    if (!ex) {
+        return;
+    }
+    ex = '';
+    result.innerHTML = '0';
+    expression.innerHTML = '';
+})
+// calculate the whole thing
+equals.addEventListener('click', () => {
+  if (!ex) {
+    result.innerHTML = '0';
+  } else {
+    ex = eval(ex);
+    expression.innerHTML += '=';
+    result.innerHTML = trim12(ex);
+  }
+})
+
+function checkLength(arg) { // if we enter a number that's too long 
+  if (arg.toString().length > 14) {
+    expression.innerHTML = 'number too long'.toUpperCase();
+    result.innerHTML = '0';
+    ex = '0';
+  } 
+}
+
+function trim12(arg) { // if we calculate a number that's too long
+  if (arg.toString().length > 14) {
+    ex = parseFloat(arg.toPrecision(12));
+    if (ex.toString().length > 14) { 
+      ex = ex.toExponential(9);
+    };
+    return ex;
+  } else {
+    return arg;
+  }
+}
